434c1b9bc2b435cf6b983b0b02b6902fa471e76e
daniel-akka <daniel_akka@hotmail.com>
2025-03-14 15:26:04 -0300

    extrair estoque

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import configEmail from "./pages/config-email/config-email.mjs";
 import extrairEstoque from "./pages/extrair-estoque/extrair-estoque.mjs";
 import home from "./pages/home/home.mjs";
-import "./extrair-estoque.mjs"
 
 const main = document.getElementById("main");
 
