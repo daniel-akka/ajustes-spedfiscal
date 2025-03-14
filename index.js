4030f5d3774e889b82c22a74b95d9b64c029664d
daniel-akka <daniel_akka@hotmail.com>
2025-03-14 14:40:04 -0300

    Ajuste nas url

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ const init = () => {
 
     window.addEventListener("hashchange", () => {
         main.innerHTML = "";
+        console.log(window.location.hash);
         switch(window.location.hash){
             case " ": 
             main.appendChild(home());
             break;
             case "#ConfiguracoesEmail":
+                console.log('Page config');
                 main.appendChild(configEmail());
                 break;
             case "#ExtrairEstoque":
+                console.log('Page config');
                 main.appendChild(extrairEstoque());
                 break;
             default:
                 main.appendChild(home());
         }
     })
+
+    
 }
 
 window.addEventListener("load", () => {
